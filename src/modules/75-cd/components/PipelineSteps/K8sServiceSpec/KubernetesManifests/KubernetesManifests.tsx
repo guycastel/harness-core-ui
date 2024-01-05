f0e65d2253e9c0cbcf293b9c10e54796db2c8dc9
shivanand-harness <148759765+shivanand-harness@users.noreply.github.com>
2024-01-05 13:09:13 +0530

    fix: [CDS-87941]: Missing chartVersion or any other fields of chart manifest in runtime screen (#19650)
    * fix: [CDS-87941]: Missing chartVersion or any other fields of chart manifest in runtime screen
    
    * fix: [CDS-87941]: fix failing snapshot tests
    
    * fix: [CDS-87941]: fix failing snapshot tests

--- src/modules/75-cd/components/PipelineSteps/K8sServiceSpec/KubernetesManifests/KubernetesManifests.tsx
+++ src/modules/75-cd/components/PipelineSteps/K8sServiceSpec/KubernetesManifests/KubernetesManifests.tsx
@@ -85,11 +85,12 @@ export function KubernetesManifests(props: KubernetesManifestsProps): React.Reac
     manifestObj => get(manifestObj, 'manifest.identifier') === primaryManifestId
   )
 
-  const filteredManifests = isHelm ? filteredManifests_ : props?.template?.manifests
+  const filteredManifests =
+    isHelm && get(props, 'template.manifests.length') > 1 ? filteredManifests_ : props?.template?.manifests
 
   return (
     <div className={cx(css.nopadLeft, css.accordionSummary)} id={`Stage.${props.stageIdentifier}.Service.Manifests`}>
-      {filteredManifests?.length && !props.fromTrigger && (
+      {!!filteredManifests?.length && !props.fromTrigger && (
         <div className={css.subheading}>
           {getString('pipelineSteps.deploy.serviceSpecifications.deploymentTypes.manifests')}
         </div>
