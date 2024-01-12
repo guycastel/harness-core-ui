52c4788a871e79bad318e10bf5bf38920b780f6f
Sayantan Mondal <62999423+MondalSayantan@users.noreply.github.com>
2024-01-12 17:03:21 +0530

    fix: [CDS-88521]: drop srm and cet logos from project card (#19795)
    * fix: [CDS-88521]: drop srm and cet logos from project card
    
    * fix: [CDS-88521]: update snapshots
    
    * fix: [CDS-88521]: remove comments
    
    * fix: [CDS-88521]: reduce idp logo size
    
    * fix: [CDS-88521]: update snapshot

--- src/modules/45-projects-orgs/components/ModuleRenderer/DefaultRenderer.tsx
+++ src/modules/45-projects-orgs/components/ModuleRenderer/DefaultRenderer.tsx
@@ -9,7 +9,6 @@ import React from 'react'
 import { Layout, Icon, Text } from '@harness/uicore'
 import { FontVariation, Color } from '@harness/design-system'
 import { useStrings } from 'framework/strings'
-import { useFeatureFlags } from '@common/hooks/useFeatureFlag'
 import { isFreePlan, useLicenseStore } from 'framework/LicenseStore/LicenseStoreContext'
 import { ModuleName } from 'framework/types/ModuleName'
 import useNavModuleInfo from '@common/hooks/useNavModuleInfo'
@@ -18,7 +17,6 @@ import css from './ModuleRenderer.module.scss'
 
 const DefaultRenderer: React.FC = () => {
   const { getString } = useStrings()
-  const { CVNG_ENABLED } = useFeatureFlags()
   const { FF_LICENSE_STATE, licenseInformation } = useLicenseStore()
   const { shouldVisible } = useNavModuleInfo(ModuleName.CD)
 
@@ -32,7 +30,6 @@ const DefaultRenderer: React.FC = () => {
         <Icon name="ci-main" size={20} />
         {FF_LICENSE_STATE === LICENSE_STATE_VALUES.ACTIVE ? <Icon name="cf-main" size={20} /> : null}
         {licenseInformation['CE']?.status === LICENSE_STATE_VALUES.ACTIVE ? <Icon name="ce-main" size={20} /> : null}
-        {CVNG_ENABLED ? <Icon name="cv-main" size={20} /> : null}
         {licenseInformation['CHAOS']?.status === LICENSE_STATE_VALUES.ACTIVE ? (
           <Icon name="chaos-main" size={20} />
         ) : null}
@@ -41,14 +38,11 @@ const DefaultRenderer: React.FC = () => {
           isFreePlan(licenseInformation, ModuleName.CI)) ? (
           <Icon name="sto-color-filled" size={20} />
         ) : null}
-        {licenseInformation[ModuleName.CET]?.status === LICENSE_STATE_VALUES.ACTIVE ? (
-          <Icon name="cet" size={20} />
-        ) : null}
         {licenseInformation[ModuleName.SEI]?.status === LICENSE_STATE_VALUES.ACTIVE ? (
           <Icon name="sei-main" size={20} />
         ) : null}
         {licenseInformation[ModuleName.IDP]?.status === LICENSE_STATE_VALUES.ACTIVE ? (
-          <Icon name="idp" size={20} />
+          <Icon name="idp" size={18} />
         ) : null}
       </Layout.Horizontal>
     </Layout.Vertical>
